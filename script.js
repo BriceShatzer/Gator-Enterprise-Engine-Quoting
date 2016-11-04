793419a60cebdfa25910cd03de04041070b30d6f
Brice Shatzer <Brice@Brices-MacBook-Pro.local>
2016-11-04 17:29:57 -0500

    Added index prevention, moved styling into separate file, added access control framework.

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        camBearingsCost: 0,
-        mainBearingsCost: 0,
-        rodBearingsCost: 0 
-    },
-    computed: {
-        total: function(){
-            var val = 0;         
-            val+=parseInt(this.camBearingsCost); 
-            val+=parseInt(this.mainBearingsCost);
-            val+=parseInt(this.rodBearingsCost);
-            return val;
+// rudimentary access control 
+/*
+var accessCheck = prompt("Owner's Last Name?");
+
+if( accessCheck.toLowerCase() === 'degeeter') {
+    document.getElementById('app').style.display = 'block';
+
+
+} else { location.reload(); }
+*/
+
+    //init vue app 
+    var app = new Vue({
+        el: '#app',
+        data: {
+            camBearingsCost: 0,
+            mainBearingsCost: 0,
+            rodBearingsCost: 0 
+        },
+        computed: {
+            total: function(){
+                var val = 0;         
+                val+=parseInt(this.camBearingsCost); 
+                val+=parseInt(this.mainBearingsCost);
+                val+=parseInt(this.rodBearingsCost);
+                return val;
+            }
         }
-    }
-})
+    });
