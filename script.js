e5daaace52c1f67b33ebfd6b6e781c6856ef82fb
Brice Shatzer <Brice@Brices-MacBook-Pro.local>
2016-12-26 12:22:40 -0600

    updates

--- script.js
+++ script.js
@@ -19,11 +19,14 @@ if( accessCheck.toLowerCase() === 'degeeter') {
             crankshaftCost: 0,
             connectingRodsCost: 0,
             pistonsCost: 0,
-            pistonRingsCost: 0, 
+            pistonRingsCost: 0,
+            oilPumpCost: 0,
+            carbCost: 0,
+            intakeManifoldCost: 0,
         },
         computed: {
             total: function(){
-                var val = 0;         
+                var val = 0;
                 val+=parseInt(this.camBearingsCost); 
                 val+=parseInt(this.mainBearingsCost);
                 val+=parseInt(this.rodBearingsCost);
@@ -31,6 +34,21 @@ if( accessCheck.toLowerCase() === 'degeeter') {
                 val+=parseInt(this.connectingRodsCost);
                 val+=parseInt(this.pistonsCost);
                 val+=parseInt(this.pistonRingsCost);
+                val+=parseInt(this.oilPumpCost);    
+                val+=parseInt(this.carbCost);
+                val+=parseInt(this.intakeManifoldCost);
+                val+=35; // Valve Covers
+                val+=55; // Water Pump
+                val+=16; // Water Inlet
+                val+=99; // Pulley Set
+                val+=175; // Distributors
+                val+=89; // Plug Wires
+                val+=35; // Spark Plugs
+                val+=25; // Top End Gaskets
+                val+=130; // Starter
+                val+=150; // Consumables
+                val+=175; // Shipping
+                val+=675; // Labor 
                 return val;
             }
         }
