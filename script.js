3e0974a4f7a963831b52e72bd33e229aa3c7412d
Brice Shatzer <Brice@Brices-MacBook-Pro.local>
2016-12-29 04:37:26 -0600

    finished adding all elements

--- script.js
+++ script.js
@@ -21,12 +21,24 @@ if( accessCheck.toLowerCase() === 'degeeter') {
             pistonsCost: 0,
             pistonRingsCost: 0,
             oilPumpCost: 0,
-            carbCost: 0,
+            timingSetCost: 0,
+            camAndLifterSetCost: 0,
+            cylinderHeadCost: 325,
+            headGasketCost: 0,
+            headBoltsCost: 0,
+            pushRodsCost: 0,
+            rockerNutCost: 0,
             intakeManifoldCost: 0,
+            carbCost: 0,
         },
         computed: {
             total: function(){
                 var val = 0;
+                val+=175; // Block (core)
+                val+=450; // Block Machine Work
+                val+=90; // Block Prep (labor)
+                val+=25; // Block Prep (parts)
+                val+=150; // In-house machine work (for freshen only)
                 val+=parseInt(this.camBearingsCost); 
                 val+=parseInt(this.mainBearingsCost);
                 val+=parseInt(this.rodBearingsCost);
@@ -34,10 +46,25 @@ if( accessCheck.toLowerCase() === 'degeeter') {
                 val+=parseInt(this.connectingRodsCost);
                 val+=parseInt(this.pistonsCost);
                 val+=parseInt(this.pistonRingsCost);
-                val+=parseInt(this.oilPumpCost);    
-                val+=parseInt(this.carbCost);
-                val+=parseInt(this.intakeManifoldCost);
+                val+=parseInt(this.oilPumpCost);
+                val+=15; // Oil Filter Adapter
+                val+=14; // Oil Pump Pick-up
+                val+=13; // Intermediate Shaft
+                val+=16; // Rear Main Seal 
+                val+=75; // Oil Pan and gasket
+                val+=26; // timing cover and seal
+                val+=65; // Harmonic Balancer 
+                val+=32; // Flex plate
+                val+=parseInt(this.timingSetCost);
+                val+=parseInt(this.camAndLifterSetCost);
+                val+=35; // Shortblock Fasteners
+                val+=parseInt(this.cylinderHeadCost);
+                val+=parseInt(this.headGasketCost);
+                val+=parseInt(this.headBoltsCost);
+                val+=parseInt(this.pushRodsCost);
+                val+=parseInt(this.rockerNutCost);
                 val+=35; // Valve Covers
+                val+=parseInt(this.intakeManifoldCost);
                 val+=55; // Water Pump
                 val+=16; // Water Inlet
                 val+=99; // Pulley Set
@@ -46,6 +73,7 @@ if( accessCheck.toLowerCase() === 'degeeter') {
                 val+=35; // Spark Plugs
                 val+=25; // Top End Gaskets
                 val+=130; // Starter
+                val+=parseInt(this.carbCost);
                 val+=150; // Consumables
                 val+=175; // Shipping
                 val+=675; // Labor 
